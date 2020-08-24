8031944810e8fae806e8ed848863de9341bd97f2
kei0722 <kayare919@gmail.com>
2020-08-24 10:50:39 +0900

    課題③-4を修正

--- js/main.js
+++ js/main.js
@@ -1,131 +1,145 @@
 'use strict';
 
+const tbody = document.querySelector('tbody');
 const options = document.getElementsByName('option');
 
-const todoList = [];
+let todoList = [];
 
-const tbody = document.querySelector('tbody');
+function createTodoWorkingon() {
+	const todoWorkingon = todoList.filter( aaa => aaa.state === '作業中');
+	createTags(todoWorkingon);
+	showTodo(todoWorkingon);
+	return todoWorkingon;
+}
 
-const todoListId = todoList.length;
+function createTodoCompleted() {
+	const todoCompleted = todoList.filter( aaa => aaa.state === '完了');
+	createTags(todoCompleted);
+	showTodo(todoCompleted);
+	return todoCompleted;
+}
 
-function showTodoList() {
-	for (let i = 0; i < todoList.length; i++) {
-		todoList[i].id = i;
-		tbody.children[i].children[0].textContent = i;
-		tbody.children[i].children[1].textContent = todoList[i].comment;
+function checkRadioButton() {
+	createTodoWorkingon();
+	createTodoCompleted();
+
+	for (let i = 0; i < options.length; i++) {
+		if (options[i].checked) {
+			if (options[i].value === 'ALL') {
+				createTags(todoList);
+				showTodo(todoList);
+			} else if (options[i].value === 'WORKINGON') {
+				createTags(createTodoWorkingon());
+				showTodo(createTodoWorkingon());
+			} else if (options[i].value === 'COMPLETED') {
+				createTags(createTodoCompleted());
+				showTodo(createTodoCompleted());
+			}
+		}
 	}
 }
 
-function showButton() {
-	const statusButton = document.createElement('button');
-	const deleteButton = document.createElement('button');
+function createTags(test01) {
 
-	for (let i = 0; i < todoList.length; i++) {
-		statusButton.textContent = todoList[i].status;
-		statusButton.className = 'working-button';
-		tbody.children[i].children[2].appendChild(statusButton);
-		deleteButton.textContent = todoList[i].deleteBtn;
-		deleteButton.className = 'delete-button';
-		tbody.children[i].children[3].appendChild(deleteButton);
+	while(tbody.firstChild) {
+		tbody.removeChild(tbody.firstChild);
 	}
 
-	statusButton.addEventListener('click', e => {
-		let index = Number(e.target.closest('tr').children[0].textContent);
-
-		if (e.target.classList.contains('working-button')) {
-			todoList[index].status = '完了';
-			e.target.textContent = todoList[index].status;
-			e.target.className = 'done-button';
-
-			for (let i = 0; i < options.length; i++) {
-				if (options[i].checked) {
-					if (options[i].value === 'WORKINGON') {
-						e.target.closest('tr').style.display = "none";
-					}
-				}
-			}
-
-		} else if (e.target.classList.contains('done-button')) {
-			todoList[index].status = '作業中';
-			e.target.textContent = todoList[index].status;
-			e.target.className = 'working-button';
-
-			for (let i = 0; i < options.length; i++) {
-				if (options[i].checked) {
-					if (options[i].value === 'DONE') {
-						e.target.closest('tr').style.display = "none";
-					}
-				}
-			}
+	for (let i = 0; i < test01.length; i++) {
+		const createTr = document.createElement('tr');
+		tbody.appendChild(createTr);
+		
+		for (let i = 0; i < document.querySelectorAll('th').length; i++) {
+			createTr.appendChild(document.createElement('td'));
 		}
-	});
+	}
+}
 
-	deleteButton.addEventListener('click', e => {
-		todoList.splice(Number(e.target.closest('tr').children[0].textContent), 1);
-		tbody.removeChild(e.target.closest('tr'));
-		showTodoList();
+
+function showTodo(test02) {
+
+	for (let i = 0; i < test02.length; i++) {
+
+		while(tbody.children[i].children[2].firstChild) {
+			tbody.children[i].children[2].removeChild(tbody.children[i].children[2].firstChild);
+		}
+
+		while(tbody.children[i].children[3].firstChild) {
+			tbody.children[i].children[3].removeChild(tbody.children[i].children[3].firstChild);
+		}
+
+		tbody.children[i].children[0].textContent = test02[i].id;
+		tbody.children[i].children[1].textContent = test02[i].comment;
+
+		const stateButton = tbody.children[i].children[2].appendChild(document.createElement('button'));
+		stateButton.textContent = test02[i].state;
+
+		if (test02[i].state === '作業中') {
+			stateButton.className = 'state-button workingon-button';
+		} else {
+			stateButton.className = 'state-button completed-button';
+		}
+
+		const deleteButton = tbody.children[i].children[3].appendChild(document.createElement('button'));
+		deleteButton.textContent = test02[i].delete;
+		deleteButton.className = 'delete-button';
+	}
+
+	const getStateButton = document.getElementsByClassName("state-button");
+	for(let i = 0; i < getStateButton.length; i++) {
+		getStateButton[i].addEventListener("click", () => {
+
+			if (getStateButton[i].classList.contains('workingon-button')) {
+				getStateButton[i].classList.remove('workingon-button');
+				getStateButton[i].classList.add('completed-button');
+				test02[i].state = '完了';
+				getStateButton[i].textContent = test02[i].state;
+				checkRadioButton();
+
+			} else {
+				getStateButton[i].classList.remove('completed-button');
+				getStateButton[i].classList.add('workingon-button');
+				test02[i].state = '作業中';
+				getStateButton[i].textContent = test02[i].state;
+				checkRadioButton();
+			}
+		});
+	}
+
+	const getDeleteButton = document.getElementsByClassName("delete-button");
+	for(let i = 0; i < getDeleteButton.length; i++) {
+		getDeleteButton[i].addEventListener("click", () => {
+			todoList.splice(test02[i].id, 1);
+
+			for (let i = 0; i < todoList.length; i++) {
+				todoList[i].id = i;
+			}
+
+			checkRadioButton();
+		});
+	}
+}
+
+for (let i = 0; i < options.length; i++) {
+	options[i].addEventListener('change', () => {
+		checkRadioButton();
 	});
 }
 
 document.getElementById('add-btn').addEventListener('click', () => {
 
-	const inputValue = document.getElementById('input-task').value;
-
-	if (inputValue === '') {
+	if (document.getElementById('input-task').value === '') {
 		return;
 	}
 
-	const tr = document.createElement('tr');
-
 	const todo = {
-		id: todoListId,
-		comment: inputValue,
-		status: '作業中',
-		deleteBtn: '削除',
-	};
-
-	todoList.push(todo);
-
-	for (let i = 0; i < Object.keys(todo).length; i++) {
-		const td = document.createElement('td');
-		tbody.appendChild(tr).appendChild(td);
-
-		for (let i = 0; i < options.length; i++) {
-			if (options[i].checked) {
-				if (options[i].value === 'DONE') {
-					tr.style.display = "none";
-				}
-			}
-		}
+		id: todoList.length,
+		comment: document.getElementById('input-task').value,
+		state: '作業中',
+		delete: '削除',
 	}
 
-	showTodoList();
-	showButton();
-
+	todoList.push(todo);
 	document.getElementById('input-task').value = '';
+	checkRadioButton();
 });
-
-for (let i = 0; i < options.length; i++) {
-	options[i].addEventListener('click', e => {
-
-		if (e.target.value === 'ALL') {
-			for (let i = 0; i < todoList.length; i++) {
-				tbody.children[i].removeAttribute('style');
-			}
-		} else if (e.target.value === 'WORKINGON') {
-			for (let i = 0; i < todoList.length; i++) {
-				tbody.children[i].removeAttribute('style');
-				if (todoList[i].status === '完了') {
-					tbody.children[i].style.display = "none";
-				}
-			}
-		} else if (e.target.value === 'DONE') {
-			for (let i = 0; i < todoList.length; i++) {
-				tbody.children[i].removeAttribute('style');
-				if (todoList[i].status === '作業中') {
-					tbody.children[i].style.display = "none";
-				}
-			}
-		}
-	});
-}
